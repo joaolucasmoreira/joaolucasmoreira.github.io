621b8a1afba9fa42870c2860f0fad91f4e31d6e4
João Moreira <jl7084139@gmail.com>
2023-10-16 22:17:26 -0300

    TAREFA BOLETIM - JAVASCRIPT - Parte 1

--- src/script/script-boletim.js
+++ src/script/script-boletim.js
@@ -1,52 +1,126 @@
 let materia = { id: 0, nome: "", notas: [], total: 0, printInfo: "" };
-        let materias = [];
-        function save() {
-            let inputNotas = [];
-            let totalNotas = 0;
-            let resultados = `<b>BOLETIM</b> <br>`;
+let materias = [];
+let inputNotas = [];
 
-            document.querySelectorAll("div.input-group input").forEach(e => {
-                inputNotas.push(parseFloat(e.value))
-                totalNotas += parseFloat(e.value);
-            })
+function save() {
+  if (!verifMateAdd() && !verifVazio(2) && !verifNegativo(2)) {
+    let totalNotas = 0;
+    let resultados = `<b>BOLETIM</b> <br>`;
 
-            materias.push(
-                {
-                    id: materia.id++,
-                    nome: nomeMateria.value.toLocaleUpperCase(),
-                    notas: inputNotas,
-                    total: totalNotas,
-                    info: `<hr>
-                            RESULTADO DE: <b>${nomeMateria.value.toLocaleUpperCase()}</b> 
-                            <ul>
-                                <li><b>1º Bimestre:</b> ${inputNotas[0]}</li>
-                                <li><b>2º Bimestre:</b> ${inputNotas[1]}</li>
-                                <li><b>3º Bimestre:</b> ${inputNotas[2]}</li>
-                                <li><b>4º Bimestre:</b> ${inputNotas[3]}</li>
-                                <li>TOTAL FINAL: <b>${totalNotas}</b></li>
-                                <li>STATUS: <b>${isAprovadoOuReprovado(totalNotas)}</b></li>
-                            </ul>
-                            `
-                }
-            )
+    document.querySelectorAll("#trocar2 input").forEach((e) => {
+      inputNotas.push(parseFloat(e.value));
+      totalNotas += parseFloat(e.value);
+    });
 
-            console.log(materias)
+    if (!verifMaxNotas()) {
+      materias.push({
+        id: materia.id++,
+        nome: nomeMateria.value,
+        notas: inputNotas,
+        total: totalNotas,
+        info: `
+                           
+        RESULTADO DE: <b>${nomeMateria.value.toLocaleUpperCase()}</b>
+        <table class="table table-striped table-bordless">   
+                <tr><td><b>1º Bimestre:</b></td> <td>${inputNotas[0]}</td></tr>
+                <tr><td><b>2º Bimestre:</b></td> <td>${inputNotas[1]}</td></tr>
+                <tr><td><b>3º Bimestre:</b></td> <td>${inputNotas[2]}</td></tr>
+                <tr><td><b>4º Bimestre:</b></td> <td>${inputNotas[3]}</td></tr>
+                <tr><td>TOTAL FINAL:</td> <td><b>${totalNotas}</b></td></tr>
+                <tr><td>STATUS:</td> <td><b>${isAprovadoOuReprovado(
+                  totalNotas
+                )}</b></td>
+            </tr>
+        </table>
+                            `,
+      });
 
-            materias.forEach(e => {
-                resultados += `<br>` + e.info;
-            })
+      console.log(materias);
 
-            resultNotas.innerHTML = resultados;
-        }
-        
-        function isAprovadoOuReprovado(valor) {
-            if (valor >= 60) {
-                return "APROVADO"
-            } else {
-                return "REPROVADO"
-            }
-        }
+      materias.forEach((e) => {
+        resultados += `<br>` + e.info;
+      });
 
-        function trocar() {
-            
-        }
+      resultNotas.innerHTML = resultados;
+    }
+  }
+}
+
+function isAprovadoOuReprovado(valor) {
+  if (valor >= 60) {
+    return "APROVADO";
+  } else {
+    return "REPROVADO";
+  }
+}
+
+function salvar() {
+  if (!verifVazio(1)) {
+    document.querySelectorAll("#trocar1 input").forEach(e=>{
+        e.disabled = true
+    })
+}
+}
+
+function verifVazio(id) {
+  let inputs = document.querySelectorAll(`#trocar${id} input`);
+  let error = false;
+
+  inputs.forEach((item) => {
+    if (item.value == "") {
+      error = true;
+    }
+  });
+  if (error) {
+    alert("Preencha todos os campos!");
+  }
+  return error;
+}
+
+function verifNegativo(id) {
+  let inputs = document.querySelectorAll(`#trocar${id} input`);
+  let error = false;
+
+  inputs.forEach((item) => {
+    if (item.value < 0) {
+      error = true;
+    }
+  });
+  if (error) {
+    alert("Não pode haver valor negativo!");
+  }
+  return error;
+}
+
+function verifMaxNotas() {
+  for (let i = 0; i < inputNotas.length; i++) {
+    if (i == 0 && inputNotas[0] > 20) {
+      alert("O 1° Bimestre tem valor máximo de 20!");
+      return true;
+    } else if (i == 1 && inputNotas[1] > 25) {
+      alert("O 2° Bimestre tem valor máximo de 25!");
+      return true;
+    } else if (i == 2 && inputNotas[2] > 25) {
+      alert("O 3° Bimestre tem valor máximo de 25!");
+      return true;
+    } else if (i == 3 && inputNotas[3] > 30) {
+      alert("O 4° Bimestre tem valor máximo de 30!");
+      return true;
+    }
+  }
+  return false;
+}
+
+function verifMateAdd() {
+  let select = document.querySelector("select").value;
+  let erro = false;
+  materias.forEach((element) => {
+    if (element.nome == select) {
+      alert("Esta matéria já foi adicionada!");
+      erro = true;
+    }
+  });
+  return erro;
+}
+
+
